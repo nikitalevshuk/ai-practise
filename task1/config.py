7d9f8cd98eb500793a6e16eaf932f4b4c4925bf0
nikitalevshuk <levshukn@gmail.com>
2025-03-03 19:52:56 +0300

    Added config.py with bot settings

---
+++ task1/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    """
+    Тут находятся настройки которые относятся к моему тг боту
+    Настройки подгружаются из переменных окружения, которые находятся в файле .env
+    Через переменную model_config я показываю своему классу откуда подтягивать переменные
+    без load_dotenv() SettingsConfigDict() не найдет мой файл .env
+    """
+    BOT_TOKEN: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+bot_settings = Settings()
