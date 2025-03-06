b842eb488ffcb0b4a0e3b6b73cb947780c9e3483
nikitalevshuk <levshukn@gmail.com>
2025-03-06 20:59:03 +0300

    added procfile and made some changes in config.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: task1/main.py
--- task1/config.py
+++ task1/config.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
-from pydantic import ConfigDict
-
+import os
 load_dotenv()
 
 class BotSettings(BaseSettings):
@@ -11,31 +10,22 @@ class BotSettings(BaseSettings):
     Через переменную model_config я показываю своему классу откуда подтягивать переменные
     без load_dotenv() SettingsConfigDict() не найдет мой файл .env
     """
-    BOT_TOKEN: str
-    OPENAI_API_KEY: str
-    OPENAI_ASSISTANT_ID: str
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_ASSISTANT_ID")
+    OPENAI_ASSISTANT_ID: str = os.getenv("OPENAI_API_KEY")
 
-    model_config = ConfigDict(extra="ignore")
+    class Config:
+        env_file = ".env"
 
 class DatabaseSettings(BaseSettings):
     """
     Тут мы храним настройки БДшки, ссылку создаем через property чтобы она генерировалась
     динамически
     """
-    DB_USER: str
-    DB_PASS: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
-    @property
-    def DATABASE_URL_asyncpg(self):
-        return (
-            f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}'
-            f'@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
-        )
-
-    model_config = ConfigDict(extra="ignore")
+    class Config:
+        env_file = ".env"
 
 bot_settings = BotSettings()
 db_settings = DatabaseSettings()
