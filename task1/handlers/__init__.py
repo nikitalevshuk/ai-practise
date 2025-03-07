de19c1c85f7b54e88bbbd29d1f658c525116054e
nikitalevshuk <levshukn@gmail.com>
2025-03-08 01:41:26 +0300

    added image handler to dispatcher

--- task1/handlers/__init__.py
+++ task1/handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Dispatcher
 from task1.handlers.voice_handler import register_voice_handler
 from task1.handlers.start_handler import register_start_handler
+from task1.handlers.image_handler import register_image_handler
 
 def register_handlers(dp:Dispatcher):
     register_voice_handler(dp)
     register_start_handler(dp)
+    register_image_handler(dp)
 
