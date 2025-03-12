5203a2a136f94d2116486f920b009244287f9dd7
nikitalevshuk <levshukn@gmail.com>
2025-03-12 20:44:38 +0300

    deleted useless logs

--- task1/bot.py
+++ task1/bot.py
@@ -4,11 +4,9 @@ from aiogram.fsm.storage.redis import RedisStorage, Redis
 from task1.logger import logger
 from task1.config import settings
 
-logger.info(settings.REDIS_URL)
 redis_client = Redis.from_url(settings.REDIS_URL)
-logger.info(redis_client)
 redis_storage = RedisStorage(redis=redis_client)
-logger.info(redis_storage)
+
 
 bot = Bot(settings.BOT_TOKEN)
 dp = Dispatcher(storage=redis_storage)
